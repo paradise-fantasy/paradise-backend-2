22dcdc6e919d79f9b502eb0e0443b4e5b6c03f56
khrall <raymitoro@gmail.com>
2016-10-06 16:40:28 +0200

    More and less output

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 # Kill node
+echo "Attempting to kill node"
 pid=$(ps aux | grep node | awk '{ print $2 }')
 kill -9 $pid
 
 # Wipe modules
+echo "Wiping modules"
 rm -rf node_modules
 
-# Write a message
-echo "Hey man" >> deploy.log
-
 # Install
+echo "Installing project"
 npm install
+
+# Build
+echo "Building project"
 npm run build
+
+# Start
+echo "Starting the server"
 npm start
